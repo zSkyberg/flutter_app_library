a48d221a218a67d7abfb07b86202e0e042c506e4
zSkyberg <gokberk_159@hotmail.com>
2021-02-11 21:51:00 +0300

    api connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,407 @@
+// import 'dart:async';
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
+// import 'package:flutter/material.dart';
+// import 'dart:convert';
+//
+// Welcome welcomeFromJson(String str) => Welcome.fromJson(json.decode(str));
+//
+// String welcomeToJson(Welcome data) => json.encode(data.toJson());
+//
+// class Welcome {
+//   Welcome({
+//     this.data,
+//     this.timestamp,
+//   });
+//
+//   Data data;
+//   int timestamp;
+//
+//   factory Welcome.fromJson(Map<String, dynamic> json) => Welcome(
+//     data: Data.fromJson(json["data"]),
+//     timestamp: json["timestamp"],
+//   );
+//
+//   Map<String, dynamic> toJson() => {
+//     "data": data.toJson(),
+//     "timestamp": timestamp,
+//   };
+// }
+// void main()=>runApp(new MyApp());
+// Future<Data> fetchData() async {
+//   var url = 'https://api.coincap.io/v2/assets/bitcoin';
+//     final response = await http.get(url);
+//     print(response.body);
+//   // Map<String, dynamic> json = jsonDecode(response.body);
+//   print(response.statusCode);
+//   if (response.statusCode == 200) {
+//     print(response.body);
+//     // If the server did return a 200 OK" response,
+//     // then parse the JSON.
+//     return Data.fromJson(jsonDecode(response.body));
+//   } else {
+//     // If the server did not return a 200 OK response,
+//     // then throw an exception.
+//     throw Exception('Failed to load album');
+//
+//   }
+//
+// }
+// class Data {
+//   Data({
+//     this.id,
+//     this.rank,
+//     this.symbol,
+//     this.name,
+//     this.supply,
+//     this.maxSupply,
+//     this.marketCapUsd,
+//     this.volumeUsd24Hr,
+//     this.priceUsd,
+//     this.changePercent24Hr,
+//     this.vwap24Hr,
+//     this.explorer,
+//   });
+//   String id;
+//   String rank;
+//   String symbol;
+//   String name;
+//   String supply;
+//   String maxSupply;
+//   String marketCapUsd;
+//   String volumeUsd24Hr;
+//   String priceUsd;
+//   String changePercent24Hr;
+//   String vwap24Hr;
+//   String explorer;
+//
+//   factory Data.fromJson(Map<String, dynamic> json) => Data(
+//     id: json["id"],
+//     rank: json["rank"],
+//     symbol: json["symbol"],
+//     name: json["name"],
+//     supply: json["supply"],
+//     maxSupply: json["maxSupply"],
+//     marketCapUsd: json["marketCapUsd"],
+//     volumeUsd24Hr: json["volumeUsd24Hr"],
+//     priceUsd: json["priceUsd"],
+//     changePercent24Hr: json["changePercent24Hr"],
+//     vwap24Hr: json["vwap24Hr"],
+//     explorer: json["explorer"],
+//   );
+//
+//   Map<String, dynamic> toJson() =>  {
+//     "id": id,
+//     "rank": rank,
+//     "symbol": symbol,
+//     "name": name,
+//     "supply": supply,
+//     "maxSupply": maxSupply,
+//     "marketCapUsd": marketCapUsd,
+//     "volumeUsd24Hr": volumeUsd24Hr,
+//     "priceUsd": priceUsd,
+//     "changePercent24Hr": changePercent24Hr,
+//     "vwap24Hr": vwap24Hr,
+//     "explorer": explorer,
+//   };
+// }
+//
+//
+// class deneme {
+//   deneme(){
+//     print('a');
+//   }
+//
+// }
+// class MyApp extends StatefulWidget {
+//   @override
+//   MyApp({Key key, this.den}) : super(key: key);
+//   final Data den;
+//
+//   _MyAppState createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//
+//   Data datalar = Data();
+//   deneme quizloader = deneme();
+//   Future<Data> futureData;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     futureData = fetchData();
+//
+//   }
+//     // Future<List> LoadQuestions() async {
+//     //   var url =
+//     //       'https://api.coincap.io/v2/assets/bitcoin';
+//     //   var response = await http.get(url);
+//     //   Map<String, dynamic> json = jsonDecode(response.body);
+//     //
+//     //   print(json['name']);
+//     //   var btc = json["data"]["id"];
+//     //   print(btc);
+//     //   return jsonDecode(response.body);
+//     // }
+//
+//
+//
+//   Widget build(BuildContext context) {
+//     // return MaterialApp(
+//     //     home: Scaffold(
+//     //       appBar: AppBar(
+//     //         // title: Text('$datalar[Data][id]'),
+//     //       ),
+//     //       body:Column(
+//     //         children: [
+//     //           Container(
+//     //             child: Center(
+//     //               child:
+//     //               Text('${}'
+//     //               ),
+//     //             )
+//     //           ),
+//     //           RaisedButton(
+//     //             onPressed:()=>{
+//     //               LoadQuestions(),
+//     //             }
+//     //           )
+//     //
+//     //         ]
+//     //       )
+//     //
+//     //     )
+//     // );  Future<String> _calculation = Future<String>.delayed(
+//     //     Duration(seconds: 2),
+//     //         () => 'Data Loaded',
+//     //   );
+//     return MaterialApp(
+//       title: 'Fetch Data Example',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Fetch Data Example'),
+//         ),
+//         body: Center(
+//           child: FutureBuilder<Data>(
+//             future: futureData,
+//             builder: (context, AsyncSnapshot<Data>snapshot) {
+//               if (snapshot.hasData) {
+//
+//                 return Text("${snapshot.data}");
+//               } else if (snapshot.hasError) {
+//                 return Text("${snapshot.error}");
+//               }
+//               // By default, show a loading spinner.
+//               return CircularProgressIndicator();
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//
+//   }
+//
+//
+// }
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(MyApp());
+Welcome welcomeFromJson(String str) => Welcome.fromJson(json.decode(str));
+
+String welcomeToJson(Welcome data) => json.encode(data.toJson());
+
+class Welcome {
+  Welcome({
+    this.data,
+    this.timestamp,
+  });
+
+  Data data;
+  int timestamp;
+
+  factory Welcome.fromJson(Map<String, dynamic> json) => Welcome(
+    data: Data.fromJson(json["data"]),
+    timestamp: json["timestamp"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "data": data.toJson(),
+    "timestamp": timestamp,
+  };
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+void main()=>runApp(new MyApp());
+Future<Data> fetchData() async {
+  var url = 'https://api.coincap.io/v2/assets/bitcoin';
+  final response = await http.get(url);
+  print(response.body);
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    Map<String, dynamic> json = jsonDecode(response.body);
+    print(json['data']['id']);
+    return Data.fromJson(json['data']);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
   }
 
+}
+
+class Data {
+
+  Data({
+    this.id,
+    this.rank,
+    this.symbol,
+    this.name,
+    this.supply,
+    this.maxSupply,
+    this.marketCapUsd,
+    this.volumeUsd24Hr,
+    this.priceUsd,
+    this.changePercent24Hr,
+    this.vwap24Hr,
+    this.explorer,
+  });
+  String id;
+  String rank;
+  String symbol;
+  String name;
+  String supply;
+  String maxSupply;
+  String marketCapUsd;
+  String volumeUsd24Hr;
+  String priceUsd;
+  String changePercent24Hr;
+  String vwap24Hr;
+  String explorer;
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+    id: json["id"],
+    rank: json["rank"],
+    symbol: json["symbol"],
+    name: json["name"],
+    supply: json["supply"],
+    maxSupply: json["maxSupply"],
+    marketCapUsd: json["marketCapUsd"],
+    volumeUsd24Hr: json["volumeUsd24Hr"],
+    priceUsd: json["priceUsd"],
+    changePercent24Hr: json["changePercent24Hr"],
+    vwap24Hr: json["vwap24Hr"],
+    explorer: json["explorer"],
+  );
+
+  Map<String, dynamic> toJson() =>  {
+    "id": id,
+    "rank": rank,
+    "symbol": symbol,
+    "name": name,
+    "supply": supply,
+    "maxSupply": maxSupply,
+    "marketCapUsd": marketCapUsd,
+    "volumeUsd24Hr": volumeUsd24Hr,
+    "priceUsd": priceUsd,
+    "changePercent24Hr": changePercent24Hr,
+    "vwap24Hr": vwap24Hr,
+    "explorer": explorer,
+  };
+}
+
+
+class MyApp extends StatefulWidget {
   @override
+  MyApp({Key key}) : super(key: key);
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Data datalar = Data();
+  Future<Data> futureData;
+
+  @override
+  void initState() {
+    super.initState();
+    futureData = fetchData();
+  }
+  // Future<List> LoadQuestions() async {
+  //   var url =
+  //       'https://api.coincap.io/v2/assets/bitcoin';
+  //   var response = await http.get(url);
+  //   Map<String, dynamic> json = jsonDecode(response.body);
+  //
+  //   print(json['name']);
+  //   var btc = json["data"]["id"];
+  //   print(btc);
+  //   return jsonDecode(response.body);
+  // }
+
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+    // return MaterialApp(
+    //     home: Scaffold(
+    //       appBar: AppBar(
+    //         // title: Text('$datalar[Data][id]'),
+    //       ),
+    //       body:Column(
+    //         children: [
+    //           Container(
+    //             child: Center(
+    //               child:
+    //               Text('${}'
+    //               ),
+    //             )
+    //           ),
+    //           RaisedButton(
+    //             onPressed:()=>{
+    //               LoadQuestions(),
+    //             }
+    //           )
     //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    //         ]
+    //       )
+    //
+    //     )
+    // );
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Data>(
+            future: futureData,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                print("sadsada");
+                print(futureData);
+                return Text("${snapshot.data.name}")
+                ;
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              // By default, show a loading spinner.
+              return CircularProgressIndicator();
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+
   }
+
+
 }
